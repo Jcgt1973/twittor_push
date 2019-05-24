a93b832084f0f2f5c636f68b9fbf77c3ca5c1ca7
Jose Carlos Garcia Torres <er__yo@hotmail.com>
2019-05-24 18:33:41 +0200

    Prueba en dispositivo fisico

---
+++ server/routes.js
@@ -0,0 +1,88 @@
+// Routes.js - Módulo de rutas
+const express = require('express');
+const router = express.Router();
+const push = require('./push');
+
+
+
+const mensajes = [
+
+  {
+    _id: 'XXX',
+    user: 'spiderman',
+    mensaje: 'Hola Mundo'
+  }
+
+];
+
+
+// Get mensajes
+router.get('/', function (req, res) {
+  // res.json('Obteniendo mensajes');
+  res.json( mensajes );
+});
+
+
+// Post mensaje
+router.post('/', function (req, res) {
+  
+  const mensaje = {
+    mensaje: req.body.mensaje,
+    user: req.body.user
+  };
+
+  mensajes.push( mensaje );
+
+  console.log(mensajes);
+
+
+  res.json({
+    ok: true,
+    mensaje
+  });
+});
+ // Alamcenar suscripción
+
+router.post('/subscribe', (req, res) => {
+  
+  const suscripcion = req.body;
+
+  // console.log(suscripcion);
+
+  push.addSubscription( suscripcion );
+  
+  res.json('subscribe');
+  
+
+});
+
+ // Alamcenar suscripción
+
+ router.get('/key', (req, res) => {
+  
+  const key = push.getKey();
+
+  res.send(key);
+
+});
+
+ // Enviar notificacion PUSH a los que queramos
+// Se controla desde el lado del server
+
+ router.post('/push', (req, res) => {
+  
+  const post = {
+    titulo: req.body.titulo,
+    cuerpo: req.body.cuerpo,
+    usuario: req.body.usuario
+  };
+
+  push.sendPush( post );
+  res.json(post);
+
+});
+
+
+
+
+module.exports = router;
