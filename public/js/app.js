8c6897cb3d631360e0b30b4c33ae842148a5b62d
Jose Carlos Garcia Torres <er__yo@hotmail.com>
2019-05-29 09:08:55 +0200

    Cambio en funcion getMessage de la ruta de los archivos

--- public/js/app.js
+++ public/js/app.js
@@ -181,7 +181,7 @@ postBtn.on('click', function() {
 // Obtener mensajes del servidor
 function getMensajes() {
 
-    fetch('https://github.com/Jcgt1973/twittor_push')
+    fetch('https://github.com/Jcgt1973/twittor_push/api')
         .then( res => res.json() )
         .then( posts => {
 
